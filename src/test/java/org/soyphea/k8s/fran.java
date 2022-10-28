80d62b0bd1ae23c44f8735107988c21c678eeb63
Franccesca Epifani <58118827+keila713@users.noreply.github.com>
2022-10-27 21:20:18 -0300

    prueba 2

--- src/test/java/org/soyphea/k8s/fran.java
+++ src/test/java/org/soyphea/k8s/fran.java
@@ -1,3 +1,19 @@
+package org.soyphea.k8s;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.soyphea.k8s.config.UserConfig;
+import org.soyphea.k8s.controller.Controller;
+import org.soyphea.k8s.srevice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 /* String mes = "enero";
  */
 
