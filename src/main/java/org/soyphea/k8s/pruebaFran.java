1a5f917350f4cd5b9aa2f513f7ae863e5a6ef962
Franccesca Epifani <58118827+keila713@users.noreply.github.com>
2022-10-28 11:50:08 -0300

    vulnerabilities

--- src/main/java/org/soyphea/k8s/pruebaFran.java
+++ src/main/java/org/soyphea/k8s/pruebaFran.java
@@ -8,12 +8,7 @@ public class test {
       try
       {
         Cipher c1 = Cipher.getInstance("DES"); 
-        Cipher c7 = Cipher.getInstance("DESede"); 
-        Cipher c13 = Cipher.getInstance("RC2"); 
-        Cipher c19 = Cipher.getInstance("RC4"); 
-        Cipher c25 = Cipher.getInstance("Blowfish"); 
 
-        NullCipher nc = new NullCipher();
       }
       catch(NoSuchAlgorithmException|NoSuchPaddingException e)
       {
