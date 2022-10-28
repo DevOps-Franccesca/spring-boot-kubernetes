63aa790bbc61c08d66b1787af8ab4c20ed7a7dda
Franccesca Epifani <58118827+keila713@users.noreply.github.com>
2022-10-28 12:00:50 -0300

    ejemplo

--- src/main/java/org/soyphea/k8s/pruebaFran.java
+++ src/main/java/org/soyphea/k8s/pruebaFran.java
@@ -1,17 +1,22 @@
-import javax.crypto.Cipher;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.NoSuchPaddingException;
-
-public class test {
-
-    public static void main(String[] args) {
-      try
-      {
-        Cipher c1 = Cipher.getInstance("DES"); 
-
-      }
-      catch(NoSuchAlgorithmException|NoSuchPaddingException e)
-      {
-      }
+import java.security.*;
+import java.util.*;
+  
+public class GFG1 {
+    public static void main(String[] argv)
+    {
+        try {
+            SecureRandom sr = new SecureRandom();
+            sr.setSeed(123456L); 
+            int v = sr.next(32);
+        }
+  
+        catch (NoSuchAlgorithmException e) {
+  
+            System.out.println("Exception thrown : " + e);
+        }
+        catch (ProviderException e) {
+  
+            System.out.println("Exception thrown : " + e);
+        }
     }
 }
